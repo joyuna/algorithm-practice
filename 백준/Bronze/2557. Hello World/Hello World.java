9beaec6ece64164d011355ec98c3bc692d7262bd
joyounga <joayounga@gmail.com>
2022-11-30 00:58:33 +0900

    22.11.28 Title: Hello World
    디렉토리 구조 변경

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -3,9 +3,6 @@
  * https://www.acmicpc.net/problem/2557
  * Q1.Hello World!를 출력하시오.
  */
-
-package y2022.m11;
-
 public class D28A1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
