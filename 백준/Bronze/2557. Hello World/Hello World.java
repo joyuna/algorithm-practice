5e2715c06fed623fc9af984843661e724e1362e1
joyounga <joayounga@gmail.com>
2022-11-30 11:40:04 +0900

    22.11.28 Title: Hello World
    클래스명 변경

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -3,7 +3,7 @@
  * https://www.acmicpc.net/problem/2557
  * Q1.Hello World!를 출력하시오.
  */
-public class D28A1 {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
