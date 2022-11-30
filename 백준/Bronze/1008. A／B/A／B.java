5d2aa7fbbc75693cc7b2dcac58720f9066b629b5
Younga <joayounga@gmail.com>
2022-11-30 23:31:27 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A / B);
+
+    }
+}
