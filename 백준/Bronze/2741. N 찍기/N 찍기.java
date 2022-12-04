52f0d8b793d6eafd6db2da1174bd8acdab230e2e
Younga <joayounga@gmail.com>
2022-12-04 23:44:41 +0900

    [Bronze V] Title: N 찍기, Time: 940 ms, Memory: 30136 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+            
+        }
+    }
+}
