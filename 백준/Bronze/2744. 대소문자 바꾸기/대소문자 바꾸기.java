14692435c9381264d876f94be056dd3c3c27d945
joyounga <joayounga@gmail.com>
2022-12-21 00:42:20 +0900

    22.12.20 Title: 대소문자 바꾸기
    개인 코멘트 추가

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String str = br.readLine();
 
         for(int i = 0; i < str.length(); i++) {
@@ -15,8 +14,34 @@ public class Main {
                 System.out.print(String.valueOf(c).toLowerCase());
             } else {
                 System.out.print(String.valueOf(c).toUpperCase());
-                
             }
         }
     }
 }
+
+/* ver2.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase());
+            } else {
+                sb.append(Characher.toUpperCase());
+            }
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
+*/
