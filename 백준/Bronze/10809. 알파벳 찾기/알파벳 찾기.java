924639d9461c2afc0d661e24f3449b85e34c3ca4
joyounga <joayounga@gmail.com>
2022-12-26 23:43:07 +0900

    22.12.24 Title: 알파벳 찾기
    개인 코멘트 추가

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -14,3 +14,34 @@ public class Main {
         System.out.println(sb);
     }
 }
+
+/* ver2.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algorithm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+        for (int var : arr) {
+            System.out.println(var + " ");
+        }
+    }
+}
+ */
