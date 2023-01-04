81e04f44d852e5f5561147b913688b6a87a5feb5
Younga <joayounga@gmail.com>
2023-01-04 22:08:08 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[8];
+        int ascending = 0;
+        int descending = 0;
+
+        for (int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+            if(i + 1 == arr[i]) {
+                ascending++;
+            } else if(8 - i == arr[i]) {
+                descending++;
+            }
+        }
+        if(ascending == 8) {
+            bw.write("ascending");
+        }
+        else if(descending == 8) {
+            bw.write("descending");
+        }
+        else {
+            bw.write("mixed");
+        }
+        br.close();
+        bw.close();
+    }
+}
