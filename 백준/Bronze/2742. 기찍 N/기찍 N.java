82379bd4fd7704e5e66d227bafe98139aa21a00d
joyounga <joayounga@gmail.com>
2023-01-07 23:32:41 +0900

    [Bronze IV] Title: 기찍 N, Time: 1056 ms, Memory: 31560 KB -BaekjoonHub
    코드 추가

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        sc.close();
 
         for(int i = 0; i < N; i++) {
             System.out.println(N - i);
