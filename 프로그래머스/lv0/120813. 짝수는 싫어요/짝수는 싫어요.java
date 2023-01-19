c73b8f32f734d92aa1a867deb5119ea66fff5949
Younga <joayounga@gmail.com>
2023-01-19 23:45:35 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        if(n % 2 == 0) {
+            answer = new int[n / 2];
+        } else {
+            answer = new int[n / 2 + 1];
+        }
+        for(int i = 0; i < n; i++) {
+            if((i + 1) % 2 == 1) {
+                answer[i / 2] = i + 1;
+            }
+        }
+        return answer;
+    }
+}
