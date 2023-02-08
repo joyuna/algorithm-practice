258e17406f74056c804e4ce99ca668ec13bad223
Younga <joayounga@gmail.com>
2023-02-09 00:52:51 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-        answer = rsp.replace("2", "7");
-        answer = answer.replace("5", "2");
-        answer = answer.replace("0", "5");
-        answer = answer.replace("7", "0");
+        answer = rsp.replace("2", "7").replace("5", "2").replace("0", "5").replace("7", "0");
         return answer;
     }
 }
