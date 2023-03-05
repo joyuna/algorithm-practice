ffd2ecb54f2d614f3ea445ccc6beb08baa4b4ddb
Younga <joayounga@gmail.com>
2023-03-05 09:07:23 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.13 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,11 +1,17 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
+        String[] tmp = new String[1];
+        StringBuilder sb = new StringBuilder();
         String[] arr = my_string.split("");
-        String tmp = arr[num1];
+        tmp[0]= arr[num1];
         arr[num1] = arr[num2];
-        arr[num2] = tmp;
-        answer = String.join("", arr);
+        arr[num2] = tmp[0];
+        
+        for(int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+        answer = sb.toString();
         return answer;
     }
 }
