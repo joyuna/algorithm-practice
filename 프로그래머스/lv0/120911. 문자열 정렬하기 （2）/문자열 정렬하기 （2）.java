34c39986969fa259253a57acae2571cfe60dbb87
Younga <joayounga@gmail.com>
2023-03-10 23:19:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.49 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string = my_string.toLowerCase();
+        char[] charArr = my_string.toCharArray();
+        Arrays.sort(charArr);
+        answer = String.valueOf(charArr);
+        return answer;
+    }
+}
