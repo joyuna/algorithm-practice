15a36e44251a72c4dfcb6b8465c0b748ddb1e3dc
Younga <joayounga@gmail.com>
2023-03-24 16:53:21 +0900

    [level 0] Title: k의 개수, Time: 432.31 ms, Memory: 373 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        String str = null;
+        for(int l = i; i <= j; i++) {
+            str += String.valueOf(i);
+        }
+        String[] arr = str.split("");
+        for(int m = 0; m < arr.length; m++) {
+            if(arr[m].equals(String.valueOf(k))) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
