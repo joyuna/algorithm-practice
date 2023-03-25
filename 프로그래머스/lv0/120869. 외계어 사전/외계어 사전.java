2afae719b78b242d115615a22a6fdde25796723c
Younga <joayounga@gmail.com>
2023-03-25 15:06:08 +0900

    [level 0] Title: 외계어 사전, Time: 0.38 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 0;
+        Arrays.sort(spell);
+        
+        for(int i = 0 ;i < dic.length; i++) {
+            String dicValue = dic[i];
+            String[] dicValueArr = dicValue.split("");
+            Arrays.sort(dicValueArr);
+            if(Arrays.equals(spell, dicValueArr)) {
+                answer = 1;
+            } else {
+                continue;
+            }
+        } 
+        answer = answer != 0 ? 1 : 2;
+        return answer;
+    }
+}
