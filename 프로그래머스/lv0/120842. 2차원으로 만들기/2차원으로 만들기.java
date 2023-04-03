296024b4e18e029e530887250ef360457e58de28
Younga <joayounga@gmail.com>
2023-04-03 23:28:34 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -2,12 +2,12 @@ class Solution {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = {};
         answer = new int[num_list.length / n][n];
-        int count = 0;
+        int index = 0;
         
         for(int i = 0; i < num_list.length / n; i++) {
             for(int j = 0; j < n; j++) {
-                answer[i][j] = num_list[count];
-                count++;
+                answer[i][j] = num_list[index];
+                index++;
             }
         }
         return answer;
